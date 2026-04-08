bb7ad6345d3879b158249923e89da84235eed0f4
minarakic <minarakic03@gmail.com>
2026-04-08 20:35:01 +0200

    Added some needed tests

---
+++ user-service/src/test/java/com/perfectevent/user_service/UserDtoTest.java
@@ -0,0 +1,19 @@
+package com.perfectevent.user_service;
+
+import com.perfectevent.user_service.dto.UserDto;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDtoTest {
+
+    @Test
+    void shouldSetAndGetName() {
+        UserDto dto = new UserDto();
+
+        dto.setName("Mina");
+        assertEquals("Mina", dto.getName());
+
+        dto.setName(null);
+        assertNull(dto.getName());
+    }
+}
